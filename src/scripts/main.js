4805afb35ec4e86d5e652a669d424effd2f3e981
Volodymyr Kolosov <kolosovvova0@gmail.com>
2025-08-28 13:39:16 +0300

    gallery

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const pictureContainer = document.querySelector('#thumbs');
+const largeImg = document.querySelector('#largeImg');
+
+pictureContainer.addEventListener('click', (ev) => {
+  const imgLink = ev.target.closest('.list-item__link');
+
+  if (imgLink) {
+    ev.preventDefault();
+
+    const newLargeImg = imgLink.getAttribute('href');
+
+    largeImg.setAttribute('src', newLargeImg);
+  }
+});
